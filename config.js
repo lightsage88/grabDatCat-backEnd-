5149ec00754018ac67c3c335b4ac74b82b12c20c
lightsage88 <adrian.e.rosales@gmail.com>
2018-01-31 00:31:28 -0800

    going to bed now

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/grabdatcat';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://<lightsage88>:<Walruses8>@ds119688.mlab.com:19688/grabdatcat';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'felixTheCat';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
